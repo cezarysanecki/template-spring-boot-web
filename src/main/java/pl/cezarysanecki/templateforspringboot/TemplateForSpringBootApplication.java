3c3049da1a59ab85d7495cb1668eebf0b9c80ab9
Cezary Sanecki <saneckicezary@gmail.com>
2023-08-06 23:13:58 +0200

    init commit

---
+++ src/main/java/pl/cezarysanecki/templateforspringboot/TemplateForSpringBootApplication.java
@@ -0,0 +1,13 @@
+package pl.cezarysanecki.templateforspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemplateForSpringBootApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TemplateForSpringBootApplication.class, args);
+  }
+
+}
---
+++ src/test/java/pl/cezarysanecki/templateforspringboot/TemplateForSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.cezarysanecki.templateforspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplateForSpringBootApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
