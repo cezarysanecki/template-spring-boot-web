5e2d93fa774e4e298933df545e2ce5f5d3087706
Cezary Sanecki <saneckicezary@gmail.com>
2023-08-07 16:21:08 +0200

    moved and changed naming of context int test

--- src/test/groovy/pl/cezarysanecki/templateforspringboot/ContextLoadingIntSpec.groovy
+++ src/test/groovy/pl/cezarysanecki/templateforspringboot/ContextLoadingIntSpec.groovy
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TemplateForSpringBootApplicationTests {
+class ContextLoadingIntSpec {
 
   @Test
   void contextLoads() {
